e97d9c2fb10a7f5fa8c3c143f420fd058edf705e
BYEDUCK <mateuszbajdak@gmail.com>
2018-12-29 13:23:53 +0100

    refactor

--- src/test/java/com/mbajdak/reportapp/service/ReportServiceImplIT.java
+++ src/test/java/com/mbajdak/reportapp/service/ReportServiceImplIT.java
@@ -5,10 +5,8 @@ import com.mbajdak.reportapp.repository.ReportRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
@@ -18,7 +16,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 @DataJpaTest
-class ReportServiceImplTest {
+class ReportServiceImplIT {
 
     @Autowired
     private ReportRepository reportRepository;
